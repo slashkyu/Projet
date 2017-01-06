e5b28ef12ee9ad3c11141776ac6baf81bba9219c
nan7 <zhaonan7fr@gmail.com>
2017-01-06 15:22:17 +0100

    Update header_paq.h

--- header_paq.h
+++ header_paq.h
@@ -9,21 +9,6 @@
 #include <elf.h>
 
 
-/* STRUCTURE DE STOCKAGE D'INFORMATIONS
-typedef struct {
-
-		Elf32_Ehdr *header;
-
-		Elf32_Shdr **table_section;
-
-		El32_Sym **table_Symbole;
-}Elf_stck;
-
-//Elf_stck Elf //variable globale
-
-*/
-
-
 // Vérification des 4 premiers octets du nombre magique ("7f 45 4c 46")
 int check_elf(Elf32_Ehdr * data);
 
