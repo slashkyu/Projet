002da8708548aa5816b9f280a71f3fcb138845b3
slashkyu <estellemarin26@gmail.com>
2017-01-09 12:36:09 +0100

    Add files via upload

--- fusion_paq.h
+++ fusion_paq.h
@@ -1,3 +1,5 @@
+#ifndef __FUSION_PAQ_H__
+#define __FUSION_PAQ_H__
 
 #include <stdio.h>
 #include <unistd.h>
@@ -13,15 +15,17 @@
 // STRUCTURE DE STOCKAGE D'INFORMATIONS
 typedef struct {
 
-                Elf32_Ehdr *header;
+		Elf32_Ehdr *header;
+		
+		//pointeur sur la table des sections
+		Elf32_Shdr *table_section;
 
-                //pointeur sur la table des sections
-                Elf32_Shdr *table_section;
+		//pointeur sur la table symbole
+		El32_Sym *table_Symbole;
 
-                //pointeur sur la table symbole
-                El32_Sym *table_Symbole;
-
-                //pointeur sur string table
-                char* string_table;
+		//pointeur sur string table
+		char* string_table;
 }Elf_stck;
+
+void fusion_section_table(Elf32_Ehdr * data1, Elf32_Ehdr *data2);
 
