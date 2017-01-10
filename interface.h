711865f4100c1f93c13e7161ee793573aa8bd97e
nan7 <zhaonan7fr@gmail.com>
2017-01-10 14:47:07 +0100

    STRUCTURE V2

---
+++ interface.h
@@ -0,0 +1,40 @@
+#ifndef __INTERFACE__H__
+#define __INTERFACE__H__
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <elf.h>
+
+
+
+
+
+// STRUCTURE DE STOCKAGE D'INFORMATIONS
+typedef struct Elf_Structure{
+		Elf32_Ehdr *header;
+		//pointeur sur la table des sections
+		Elf32_Shdr *table_section;
+		//pointeur sur la table symbole
+		Elf32_Sym *table_symbole;
+		
+		Elf32_Rel *table_rel;
+
+		Elf32_Rela *table_rela;
+		//pointeur sur string table de sectioin
+		char* string_table_section;
+		//pointeur sur string table de symbole
+		char* string_table_symbole;
+		
+		int nb_Section;
+
+		int nb_Symbole;
+
+		int nb_Rel;
+
+		int nb_Rela;
+}Elf32;
+
+#endif
