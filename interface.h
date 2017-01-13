da83008e353f245149860df7593627af850835c8
renfreeaaron <renfreeaaron@gmail.com>
2017-01-13 09:40:28 +0100

    v9 +++++

--- interface.h
+++ interface.h
@@ -12,34 +12,37 @@
 
 
 
-// STRUCTURE DE STOCKAGE D'INFORMATIONS
+// STRUCTURE DE STOCKAGE D'INFORMATIONS de fichier 
 typedef struct Elf_Structure{
+		//pointeur sur l'en-tête
 		Elf32_Ehdr *header;
 		//pointeur sur la table des sections
 		Elf32_Shdr *table_section;
 		//pointeur sur la table symbole
 		Elf32_Sym *table_symbole;
-		
+		//pointeur sur la table de relocation de type rel
 		Elf32_Rel *table_rel;
-
+		//pointeur sur la table de relocation de type rela
 		Elf32_Rela *table_rela;
-		//pointeur sur string table de sectioin
+		//pointeur sur string table de sections
 		char* string_table_section;
-		//pointeur sur string table de symbole
+		//pointeur sur string table de symboles
 		char* string_table_symbole;
-		unsigned char* contenue_section[2048];
-
-		Elf32_Shdr *s_h_s;//Secion Header Start pour void afficherSection(Elf32 *e)	
+		//pointeur sur string table de contenu des sections
+		unsigned char* contenue_section[1024];
+		//pointeur sur Secion Header Start
+		Elf32_Shdr *s_h_s;
+		//Nobre de sections
 		int nb_Section;
-
+		//Nombre de symboles
 		int nb_Symbole;
-
+		//Nombre de symboles dans la table de relocation de type rel
 		int nb_Rel;
-
+		//Nombre de symboles dans la table de relocation de type rela
 		int nb_Rela;
-
+		//Bool pour dire si il y a des symboles dans la table de relocation de type rel
 		int relE;
-
+		//Bool pour dire si il y a des symboles dans la table de relocation de type rela
 		int relaE;
 }Elf32;
 
