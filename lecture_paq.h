31929d777ea699e7dca634dec9f1deb13901d119
slashkyu <estellemarin26@gmail.com>
2017-01-13 10:38:42 +0100

    Add files via upload

--- lecture_paq.h
+++ lecture_paq.h
@@ -9,6 +9,7 @@
 #include <elf.h>
 
 
+
 // Variable globale pour le choix de langue
 int language;
 
@@ -18,6 +19,11 @@ void print_section_table(Elf32_Ehdr * data);
 void print_section(Elf32_Ehdr * data);
 void print_sym(Elf32_Ehdr * data);
 //ETAPES 1_2_3_4
+
+Elf32_Shdr find_section(Elf32_Ehdr *data, int type, char*string);
+
+static const char *get_section_type_name(unsigned int sh_type);
+
 void process_etapes(int file_descriptor, char sous_option);
 
 #endif
